cd53806b2c1b496deccdad3f216a8d2bb8c5f5eb
Corto-gg <artem-turkin2013@yandex.ru>
2024-08-21 00:48:56 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,20 @@
+my_dict = {"Artem": 1996, 'Anastasia': 2001, 'Andrey': 1997}
+print (my_dict)
+print (my_dict ["Artem"])
+print (my_dict.get('Anton', 'Такого ключа не существует'))
+my_dict .update({'Alina': 2003,'Kamila': 2002})
+print (my_dict)
+a = my_dict.pop('Artem')
+print (a)
+print (my_dict)
+my_set = {1, 2, 3, 1, 2, 3, 1, 2, 3, 'Lock', True, (5, 5 , 4), 'Lock', True, (5, 5, 4)}
+print (my_set)
+my_set.add( 6 )
+my_set.add( 8 )
+my_set.discard( 2 )
+print (my_set)
+
+
+
+
+
